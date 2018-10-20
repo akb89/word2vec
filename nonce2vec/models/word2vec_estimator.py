d9da2096e019c8146a39db7c552bca6e7ac169f5
akb89 <akb@3azouz.net>
2018-10-20 17:17:21 +0200

    Updated prefetching batch size

--- nonce2vec/models/word2vec_estimator.py
+++ nonce2vec/models/word2vec_estimator.py
@@ -175,7 +175,7 @@ class Word2Vec():
                 .map(lambda x: tf.strings.split([x]), num_parallel_calls=p_num_threads)
                 .map(lambda x: self._vocab.lookup(x.values), num_parallel_calls=p_num_threads)  # discretize
                 .map(lambda tokens: extract_examples(tokens, window_size, p_num_threads), num_parallel_calls=p_num_threads)
-                .prefetch(100000)
+                .prefetch(25000)
                 .flat_map(lambda features, labels: tf.data.Dataset.from_tensor_slices((features, labels)))
                 .shuffle(buffer_size=shuffling_buffer_size,
                          reshuffle_each_iteration=False)
