3c04eea6e6a4b9b5222447140d1fb222fa9475b8
akb89 <akb@3azouz.net>
2018-10-20 11:07:56 +0200

    Functional version of dataset_generation

--- test_py.py
+++ test_py.py
@@ -1,17 +1,37 @@
+import tensorflow as tf
+
 def get_ctx_items(tokens, target_id, window_size):
     ctx_1 = tokens[max(0, target_id-window_size):target_id]
     ctx_2 = tokens[target_id+1:min(len(tokens), target_id+window_size+1)]
     return [*ctx_1, *ctx_2]
 
-def get_example(training_data_filepath, window_size):
+def get_example():
+    training_data_filepath = '/Users/AKB/GitHub/nonce2vec/data/wikipedia/wiki.test'
+    window_size = 15
     with open(training_data_filepath, 'r') as training_datastream:
         for line in training_datastream:
             tokens = line.strip().split()
             for target_id, target in enumerate(tokens):
                 for ctx in get_ctx_items(tokens, target_id, window_size):
-                    yield self._word2id[target], self._word2id[ctx]
+                    #yield self._word2id[target], self._word2id[ctx]
+                    yield 0, 1
 
 if __name__ == '__main__':
     window_size = 15
     training_data_filepath = '/Users/AKB/GitHub/nonce2vec/data/wikipedia/wiki.all.utf8.sent.split.lower'
-    for 
+    # for item in get_example(training_data_filepath, window_size):
+    #     print(item)
+    with tf.Session(graph=tf.Graph()) as session:
+        dataset = (tf.data.Dataset.from_generator(get_example,
+                                               (tf.int32, tf.int32))
+                .shuffle(buffer_size=10,
+                         reshuffle_each_iteration=False)
+                .repeat(5)
+                .batch(128)
+                .prefetch(10))
+        iterator = dataset.make_initializable_iterator()
+        init_op = iterator.initializer
+        x = iterator.get_next()
+        session.run(init_op)
+        while True:
+            print(x[0].eval(), x[1].eval())
--- test_tf.py
+++ test_tf.py
@@ -1,16 +1,5 @@
 import tensorflow as tf
 
-
-def convert_to_ex(target_idx, ctx_idx, tokens):
-    target = tf.nn.embedding_lookup(tokens, target_idx)
-    ctx = tf.nn.embedding_lookup(tokens, ctx_idx)
-    # return tf.convert_to_tensor([self._word2id[target.decode('utf8')],
-    #                              self._word2id[ctx.decode('utf8')]])
-    return tf.convert_to_tensor([0, 1])
-
-def target_idxx(tokens):
-    return tf.range(start=0, limit=tf.size(tokens), delta=1)
-
 def ctx_idxx(target_idx, window_size, tokens):
     """
     if target_idx > 0 and target_idx+1 < tf.size(ctx_range):
@@ -20,86 +9,61 @@ def ctx_idxx(target_idx, window_size, tokens):
         return ctx_range[target_idx+1, tf.size(ctx_range)]
     if target_idx > 0 and target_idx+1 == tf.size(ctx_range):
         return ctx_range[0:target_idx]
-    return tf.convert_to_tensor([])
+    return tf.constant([])
     """
-    return tf.convert_to_tensor([0, 1])
-    return tf.range(start=tf.maximum(0, tf.subtract(target_idx, window_size)), limit=100, delta=1)
     ctx_range = tf.range(start=tf.maximum(0, target_idx-window_size),
                          limit=tf.minimum(tf.size(tokens),
                                           target_idx+window_size+1),
                          delta=1)
-    return ctx_range
-    t0 = lambda: tf.convert_to_tensor([], dtype=tf.int32)
+    t0 = lambda: tf.constant([], dtype=tf.int32)
     t1 = lambda: tf.concat([ctx_range[0:target_idx],
                             ctx_range[target_idx+1:tf.size(ctx_range)]],
                            axis=0)
     t2 = lambda: ctx_range[target_idx+1:tf.size(ctx_range)]
     t3 = lambda: ctx_range[0:target_idx]
     c1 = tf.logical_and(tf.greater(target_idx, 0),
-                        tf.logical_not(tf.greater_equal(target_idx+1,
-                                                        tf.size(ctx_range))))
+                        tf.less(target_idx+1, tf.size(ctx_range)))
     c2 = tf.logical_and(tf.equal(target_idx, 0),
-                        tf.logical_not(tf.greater_equal(target_idx+1,
-                                                        tf.size(ctx_range))))
+                        tf.less(target_idx+1, tf.size(ctx_range)))
     c3 = tf.logical_and(tf.greater(target_idx, 0),
                         tf.equal(target_idx+1, tf.size(ctx_range)))
     return tf.case({c1: t1, c2: t2, c3: t3}, default=t0, exclusive=True)
-    #return tf.SparseTensor(indices=[ctx_range], values=t, dense_shape=[1, tf.size(ctx_range)])
 
-def examples(tokens, target_idx, window_size):
-    t = tf.map_fn(lambda ctx_idx: convert_to_ex(target_idx, ctx_idx, tokens), ctx_idxx(target_idx, window_size, tokens), infer_shape=False)
-    #return tf.contrib.layers.flatten(t)
-    #return tf.convert_to_tensor([0,1])
-    return t
+def stack_to_features_and_labels(features, labels, target_idx, tokens):
+    ctxs = ctx_idxx(target_idx, 2, tokens)
+    label = tf.nn.embedding_lookup(tokens, ctxs)
+    feature = tf.fill([tf.size(label)], tokens[target_idx])
+    return tf.concat([features, feature], axis=0), \
+           tf.concat([labels, label], axis=0), target_idx+1, tokens
 
 def extract_examples(tokens, window_size):
-    return tf.map_fn(lambda x: tf.map_fn(lambda y: [0, 1], x), tokens)
+    features = tf.constant([], dtype=tf.int32)
+    labels = tf.constant([], dtype=tf.int32)
+    target_idx = tf.constant(0)
+    target_idx_less_than_tokens_size = lambda w, x, y, z: tf.less(y, tf.size(tokens))
+    result = tf.while_loop(
+        cond=target_idx_less_than_tokens_size,
+        body=stack_to_features_and_labels,
+        loop_vars=[features, labels, target_idx, tokens],
+        shape_invariants=[tf.TensorShape([None]), tf.TensorShape([None]),
+                          target_idx.get_shape(), tokens.get_shape()])
+    return result[0], result[1]
 
 if __name__ == '__main__':
     tf.enable_eager_execution()
     window_size = 4
-    # ctx_idxx(target_idx, window_size, tokens)
-    # tf.range(start=0, limit=tf.size(tokens), delta=1)
     with tf.Session(graph=tf.Graph()) as session:
         dataset = (tf.data.TextLineDataset('/Users/AKB/GitHub/nonce2vec/data/wikipedia/wiki.test')
                    .map(tf.strings.strip)
-                   .filter(lambda x: tf.not_equal(tf.strings.length(x), 0))
+                   .filter(lambda x: tf.not_equal(tf.strings.length(x), 0))  # Filter empty strings
                    .map(lambda x: tf.strings.split([x]))
                    .map(lambda x: x.values)
-                   #.map(lambda tokens: tf.SparseTensor(indices=[1, target_idxx(tokens)], values=tokens, dense_shape=[1])))
-                   #.map(lambda tokens: tf.map_fn(lambda target_idx: examples(tokens, target_idx, window_size), target_idxx(tokens), infer_shape=False)))
-                   .map(lambda tokens: tf.map_fn(lambda target_idx: tf.map_fn(lambda ctx_idx: convert_to_ex(target_idx, ctx_idx, tokens), ctx_idxx(target_idx, window_size, tokens), dtype=tf.int32), target_idxx(tokens), dtype=tf.int32)))
-                   #.map(lambda tokens: extract_examples(tokens, window_size)))
-                   #.map(lambda tokens: tf.map_fn(lambda target_idx: tf.map_fn(lambda ctx_idx: tf.convert_to_tensor([0, 1]), tokens, dtype=tf.int32), tokens, dtype=tf.int32)))
-        #test_ds = dataset.map(lambda tokens: tf.map_fn(lambda target_idx: target_idx, target_idxx(tokens)))
+                   .map(lambda tokens: tf.map_fn(lambda token: 0, tokens, dtype=tf.int32))  # discretized
+                   .map(lambda tokens: extract_examples(tokens, window_size))
+                   .flat_map(lambda features, labels: tf.data.Dataset.from_tensor_slices((features, labels))))
         iterator = dataset.make_initializable_iterator()
         init_op = iterator.initializer
         x = iterator.get_next()
         session.run(init_op)
-        def test_x(x):
-            return x - 2
-            #return tf.subtract(x, 2)
-            #return tf.maximum(0, tf.subtract(x, 2))
-            #return [tf.range(start=tf.maximum(0, tf.subtract(x, 2)), limit=10, delta=1)]
         while True:
-            #z = tf.convert_to_tensor([0,1,2,3])
-            #y = tf.map_fn(lambda x: tf.range(start=0, limit=tf.size(x, out_type=tf.int64), delta=1, dtype=tf.int64), tf.cast(z, tf.int64))
-            #print(y.eval())
-            #z = tf.convert_to_tensor([0,1,2])
-            #y = tf.map_fn(lambda x: test_x(x), z)
-            #print(y.eval())
-            #z = tf.range(start=tf.maximum(0, tf.subtract(y, 2)), limit=10, delta=1)
-            #print(z.eval())
-            #x.infer_shape = False
-            print(x)
-            print(x[0])
-            print(x[0][0])
-            print(x.eval())
-            #print(x.eval())
-            z = tf.reshape(tf.reshape(x, [-1, 2]), [-1, 2])
-            t = tf.reshape(z, [-1])
-            #print(z)
-            #print(t)
-            #print(z.eval())
-            #print(t.eval())
-            #print(x[0].eval(), x[1].eval())
+            print(x[0].eval(), x[1].eval())
