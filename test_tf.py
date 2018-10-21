a29c18a5ae14c27ceaa2e1459e795d4df787b264
akb89 <akb@3azouz.net>
2018-10-21 22:39:26 +0200

    Trying JIT compilation

--- test_tf.py
+++ test_tf.py
@@ -99,20 +99,20 @@ class Word2Vec():
                                   window_size.get_shape()],
                 parallel_iterations=p_num_threads)
             return result[0], result[1]
-
-        return (tf.data.TextLineDataset(training_data_filepath)
-                .map(tf.strings.strip, num_parallel_calls=p_num_threads)
-                .filter(lambda x: tf.not_equal(tf.strings.length(x), 0))  # Filter empty strings
-                .map(lambda x: tf.strings.split([x]), num_parallel_calls=p_num_threads)
-                .map(lambda x: self._vocab.lookup(x.values), num_parallel_calls=p_num_threads)  # discretize
-                .map(lambda tokens: extract_examples(tokens, window_size, p_num_threads), num_parallel_calls=p_num_threads)
-                .prefetch(flat_map_pref_batch_size)
-                .flat_map(lambda features, labels: tf.data.Dataset.from_tensor_slices((features, labels)))
-                # .shuffle(buffer_size=shuffling_buffer_size,
-                #          reshuffle_each_iteration=False)
-                .repeat(num_epochs)
-                .batch(batch_size)
-                .prefetch(prefetch_batch_size))
+        with tf.contrib.compiler.jit.experimental_jit_scope():
+            return (tf.data.TextLineDataset(training_data_filepath)
+                    .map(tf.strings.strip, num_parallel_calls=p_num_threads)
+                    .filter(lambda x: tf.not_equal(tf.strings.length(x), 0))  # Filter empty strings
+                    .map(lambda x: tf.strings.split([x]), num_parallel_calls=p_num_threads)
+                    .map(lambda x: self._vocab.lookup(x.values), num_parallel_calls=p_num_threads)  # discretize
+                    .map(lambda tokens: extract_examples(tokens, window_size, p_num_threads), num_parallel_calls=p_num_threads)
+                    .prefetch(flat_map_pref_batch_size)
+                    .flat_map(lambda features, labels: tf.data.Dataset.from_tensor_slices((features, labels)))
+                    # .shuffle(buffer_size=shuffling_buffer_size,
+                    #          reshuffle_each_iteration=False)
+                    .repeat(num_epochs)
+                    .batch(batch_size)
+                    .prefetch(prefetch_batch_size))
 
 if __name__ == '__main__':
     TDF = sys.argv[1]
