28bb4b85c179cfa1287bacd33e2d9a154f1fcead
akb89 <akb@3azouz.net>
2018-10-23 13:23:00 +0200

    misc. updates

--- test_tf.py
+++ test_tf.py
@@ -106,8 +106,7 @@ class Word2Vec():
                 .prefetch(prefetch_batch_size*batch_size)
                 .flat_map(lambda features, labels: tf.data.Dataset.from_tensor_slices((features, labels)))
                 .repeat(num_epochs)
-                .batch(batch_size)
-                .prefetch(prefetch_batch_size*batch_size))
+                .batch(batch_size))
 
 
 if __name__ == '__main__':
