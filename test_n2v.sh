37ed8bcff718313dea4709c10521d17343b0e1f6
akb89 <akb@3azouz.net>
2018-10-24 12:11:15 +0200

    Bugfix on test script

--- test_n2v.sh
+++ test_n2v.sh
@@ -11,6 +11,6 @@
 
 module load GCC/6.4.0-2.28  OpenMPI/2.1.2 Python/3.6.4 TensorFlow/1.7.0-Python-3.6.4 cuDNN/7.0.5-CUDA-9.1.85
 
-srun python3 install /home/kabbach/nonce2vec/setup.py
+srun python3 /home/kabbach/nonce2vec/setup.py install
 
 srun n2v train --data /home/kabbach/nonce2vec/data/enwiki.20180920.utf8.lower.txt --outputdir /home/kabbach/nonce2vec/models --vocab /home/kabbach/nonce2vec/models/enwiki.20180920.utf8.lower.txt.vocab --alpha 0.025 --neg 5 --window 5 --epochs 1 --size 400 --min-count 50 --sample 1e-5  --train-mode skipgram --t-num-threads 48 --p-num-threads 48 --batch 2097152
