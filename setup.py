b3a7951cc633d5c6dafbda5816bdeae2e3fbf7c7
AKB <akb@3azouz.net>
2019-04-17 22:15:08 +0200

    Removed pip install

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     author_email='akb@3azouz.net',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version='0.1.4',
+    version='0.1.5',
     url='https://github.com/akb89/word2vec',
     download_url='https://github.com/akb89/word2vec/archive/0.1.0.tar.gz',
     license='MIT',
